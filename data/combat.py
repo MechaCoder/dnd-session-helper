1235a068852b94b235659e81c3f59b0ffccdd436
Matthew Proudman <proudmandandc@gmail.com>
2021-12-18 21:27:00 +0000

    :: removed Ecounter elements

--- data/combat.py
+++ data/combat.py
@@ -2,27 +2,3 @@ from .base import BaseData
 from tinydb import Query
 from .__init__ import Screen
 
-
-class EncounterData(BaseData): 
-    
-    def __init__(self, table: str = 'encounter', requiredKeys='name:str,url:str,screenHex:int'):
-        super().__init__(table=table, requiredKeys=requiredKeys)
-
-    def create(self, name:str, url:str, screenHex:int):
-
-        if screenHex not in Screen().getListOfHexs():
-            raise TypeError('hex has to be vaild')
-
-        row = {
-            'name': name,
-            'url': url,
-            'screenHex': screenHex
-        }
-        return super().create(row)
-
-    def readByCompagn(self, campaign:int):
-        db = self.createObj()
-        rows = db.tbl.search(Query().campaign == campaign)
-        db.close()
-
-        return rows
